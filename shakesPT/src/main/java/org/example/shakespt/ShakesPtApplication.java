7c815f1656db60506776786f4d7a603ce22b3e66
jun9898 <nice1998@gmail.com>
2024-02-28 22:52:58 +0900

    Feat : 프로젝트 생성

---
+++ shakesPT/src/main/java/org/example/shakespt/ShakesPtApplication.java
@@ -0,0 +1,13 @@
+package org.example.shakespt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakesPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakesPtApplication.class, args);
+	}
+
+}
