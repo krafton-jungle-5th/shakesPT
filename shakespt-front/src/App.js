832578bc9fb65176a23a21aafaab5bbb1b1e2fa9
park-yina <kln99988@naver.com>
2024-03-05 21:25:46 +0900

    FEAT:헤더 및 app.js의 기본 세팅을 홈으로 변경

--- shakespt-front/src/App.js
+++ shakespt-front/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./components/Home"; // 여기서 home을 Home으로 수정
+import './css/Home.css';
+import './css/Home_mobile.css';
+import './css/Header.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Home />
   );
-}
+};
 
 export default App;
+
