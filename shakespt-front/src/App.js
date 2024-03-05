d4a5622960f5a7435c292f61019f82785f9c530a
jun9898 <nice1998@gmail.com>
2024-03-05 16:02:02 +0900

    Feat : React 페이지 작성중

--- shakespt-front/src/App.js
+++ shakespt-front/src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, InputGroup, Form, Card} from 'react-bootstrap'
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="mainContainer">
+          <div>
+              <div className="header">
+                  <img className="logo" src="/assets/img.png" alt="logo"/>
+                  <InputGroup className="search">
+                      <Form.Control
+                          className="search-box"
+                          aria-label="Example text with button addon"
+                          aria-describedby="basic-addon1"
+                      />
+                      <Button variant="outline-secondary" className="search-button" id="button-addon1">
+                          search
+                      </Button>
+                  </InputGroup>
+              </div>
+              <div className="content">
+                  <button className="content-container">
+                      <h3 className="content-text">이야기를 시작해주세요</h3>
+                  </button>
+                  <div>
+                  필터
+                  </div>
+
+                  <div className="content-container">
+                      <div className="wrap-vertical">
+
+                          <div className="card-container">
+                              <Card>
+                                  <Card.Img className="card-image" variant="top" src="/assets/cola.jpg"/>
+                                  <Card.Body>
+                                      <Card.Text style={{wordWrap: "normal"}}>
+                                          귀여운 강아지 한마리가 앉아있습니다
+                                      </Card.Text>
+                                  </Card.Body>
+                              </Card>
+                          </div>
+                          <div className="card-container">
+                              <Card>
+                                  <Card.Img className="card-image" variant="top" src="/assets/cola.jpg"/>
+                                  <Card.Body>
+                                      <Card.Text style={{wordWrap: "normal"}}>
+                                          귀여운 강아지 한마리가 앉아있습니다
+                                      </Card.Text>
+                                  </Card.Body>
+                              </Card>
+                          </div>
+                          <div className="card-container">
+                              <Card>
+                                  <Card.Img className="card-image" variant="top" src="/assets/cola.jpg"/>
+                                  <Card.Body>
+                                      <Card.Text style={{wordWrap: "normal"}}>
+                                          귀여운 강아지 한마리가 앉아있습니다
+                                      </Card.Text>
+                                  </Card.Body>
+                              </Card>
+                          </div>
+                          <div className="card-container">
+                              <Card>
+                                  <Card.Img className="card-image" variant="top" src="/assets/cola.jpg"/>
+                                  <Card.Body>
+                                      <Card.Text style={{wordWrap: "normal"}}>
+                                          귀여운 강아지 한마리가 앉아있습니다
+                                      </Card.Text>
+                                  </Card.Body>
+                              </Card>
+                          </div>
+                          <div>
+                              <p>5/10</p>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div className="content-container">
+                      <div className="wrap-vertical">
+                          <div className="card-container">
+                              <Card>
+                                  <Card.Img className="card-image" variant="top" src="/assets/cola.jpg"/>
+                                  <Card.Body>
+                                      <Card.Title>Card Title</Card.Title>
+                                      <Card.Text style={{wordWrap: "normal"}}>
+                                      Some quick example text to build on the card title and make up the
+                                          bulk of the card's content.
+                                      </Card.Text>
+                                  </Card.Body>
+                              </Card>
+                          </div>
+                          <div className="card-container">
+                              <Card>
+                                  <Card.Img className="card-image" variant="top" src="/assets/cola.jpg"/>
+                                  <Card.Body>
+                                      <Card.Title>Card Title</Card.Title>
+                                      <Card.Text style={{wordWrap: "normal"}}>
+                                          Some quick example text to build on the card title and make up the
+                                          bulk of the card's content.
+                                      </Card.Text>
+                                  </Card.Body>
+                              </Card>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
+
 }
 
 export default App;
