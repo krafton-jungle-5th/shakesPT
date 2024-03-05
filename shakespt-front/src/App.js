a80f1c1abc41989d3ae22f09aab7225b4649aaff
jun9898 <nice1998@gmail.com>
2024-03-05 23:04:36 +0900

    Feat : Front 페이지 작업 끝 무한 스크롤, 더미데이터 넣고 테스트 해야함

--- shakespt-front/src/App.js
+++ shakespt-front/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import {Button, Form, InputGroup} from 'react-bootstrap'
+import {Button, Dropdown, DropdownButton, Form, InputGroup} from 'react-bootstrap'
 import React from "react";
 import WrapVertical from "./components/WrapVertical";
+import { FaCameraRetro } from "react-icons/fa";
 
 function App() {
     return (
@@ -22,14 +23,23 @@ function App() {
                 </div>
                 <div className="content">
                     <button className="append-card-container">
-                        <h3 className="content-text">이야기를 시작해주세요</h3>
+                        <h3 className="content-text">당신의 이야기를 시작해주세요<FaCameraRetro/></h3>
                     </button>
-                    <div>
-                        필터
-                    </div>
+                    <DropdownButton className="dropdown-custom" id="dropdown-basic-button" title="최신순">
+                        <Dropdown.Item href="#/action-1">최신순</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">완료된 글</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">미완료된 글</Dropdown.Item>
+                    </DropdownButton>
 
                     {/*이곳에 무한 스크롤 관련 옵션을 넣어야함*/}
                     {/*서버에서 요청한 값을 map으로 돌리며 해당 component를 생성*/}
+                    <WrapVertical/>
+                    <WrapVertical/>
+                    <WrapVertical/>
+                    <WrapVertical/>
+                    <WrapVertical/>
+                    <WrapVertical/>
+                    <WrapVertical/>
                     <WrapVertical/>
                     <WrapVertical/>
                 </div>
