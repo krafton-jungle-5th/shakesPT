f4f08bc33e460954389fc9fa41780184862e1350
jun9898 <nice1998@gmail.com>
2024-03-06 16:14:37 +0900

    Feat : 모달창 제작 완료

--- shakespt-front/src/App.js
+++ shakespt-front/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {Button, Dropdown, DropdownButton, Form, InputGroup} from 'react-bootstrap'
-import React from "react";
+import React, {useState} from "react";
 import WrapVertical from "./components/WrapVertical";
-import { FaCameraRetro } from "react-icons/fa";
+import {FaCameraRetro} from "react-icons/fa";
+import VerticallyCenteredModal from "./components/VerticallyCenteredModal";
 
 function App() {
+
+    const [modalOpen, setModalOpen] = useState(false);
+
     return (
         <div className="mainContainer">
             <div>
@@ -22,9 +26,14 @@ function App() {
                     </InputGroup>
                 </div>
                 <div className="content">
-                    <button className="append-card-container">
+                    <button className="append-card-container" onClick={() => setModalOpen(true)}>
                         <h3 className="content-text">당신의 이야기를 시작해주세요<FaCameraRetro/></h3>
                     </button>
+
+                    <VerticallyCenteredModal
+                        show = {modalOpen}
+                        onHide = {() => setModalOpen(false)}
+                    />
                     <DropdownButton className="dropdown-custom" id="dropdown-basic-button" title="최신순">
                         <Dropdown.Item href="#/action-1">최신순</Dropdown.Item>
                         <Dropdown.Item href="#/action-2">완료된 글</Dropdown.Item>
@@ -33,15 +42,18 @@ function App() {
 
                     {/*이곳에 무한 스크롤 관련 옵션을 넣어야함*/}
                     {/*서버에서 요청한 값을 map으로 돌리며 해당 component를 생성*/}
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
-                    <WrapVertical/>
+                    <div>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+                        <WrapVertical/>
+
+                    </div>
                 </div>
             </div>
         </div>
