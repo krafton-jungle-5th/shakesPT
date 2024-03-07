b282707c6ebcf6c2f260d2c04780d72166a7ece5
park-yina <kln99988@naver.com>
2024-03-07 19:53:51 +0900

    Feat:대머리 제거

--- shakespt-front/src/components/Header.jsx
+++ shakespt-front/src/components/Header.jsx
@@ -4,7 +4,6 @@ import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <header className="header">
-      <img className="shakes_pt" src={shakesIcon} alt="대머리 아저씨" />
       <img className="logo" src={logo} alt="Shakes 로고" />
     </header>
   );
